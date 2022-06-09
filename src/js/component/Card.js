b341e06b99b18852071838dd621f80f3fa68532d
Josea1705 <josebarreto665@gmail.com>
2022-06-09 23:41:49 +0000

    error de classnames

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -3,12 +3,12 @@ import React from "react";
 
 export const Card = ({item}) => {
     return (
-        <div ClassName="card" style={{width: "18rem"}}>
+        <div className="card" style={{width: "18rem"}}>
             <img src={item.image} ClassName="card-img-top" alt="..."/>
-            <div ClassName="card-body">
-                <h5 ClassName="card-title">{item.name}</h5>
-                <p ClassName="card-text">{item.status}</p>
-                <a href="#" ClassName="btn btn-primary">Go somewhere</a>
+            <div className="card-body">
+                <h5 className="card-title">{item.name}</h5>
+                <p className="card-text">{item.status}</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
     );
