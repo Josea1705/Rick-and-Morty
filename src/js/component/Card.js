74100bfc87d59af7183e67205c3220a25c2a4d3a
Josea1705 <josebarreto665@gmail.com>
2022-06-09 23:42:44 +0000

    error de classnames2

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -4,7 +4,7 @@ import React from "react";
 export const Card = ({item}) => {
     return (
         <div className="card" style={{width: "18rem"}}>
-            <img src={item.image} ClassName="card-img-top" alt="..."/>
+            <img src={item.image} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{item.name}</h5>
                 <p className="card-text">{item.status}</p>
